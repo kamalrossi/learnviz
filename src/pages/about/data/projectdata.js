bb163494a740ee86f08512fabe1e413de1db2fe7
kamalrossi <rossikamal@gmail.com>
2022-06-30 22:05:40 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,52 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "LearnViz";
+const projectdesc = {
+  title: "Title",
+  desc:
+    "LearnViz is a data science dashboard for student performance management/ assessment in academic school/college. Student course, grade, attendance and progress evolution is viewable with interactive visual grapichs."
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag2: "ChartJS",
+    tag3: "D3",
+    tag4: "EdTech"
+  }
+];
+const projecttime = [
+  {
+    title: "UX/UI  Design",
+    from: "Jan 22",
+    to: "Jun 22"
+  },
+  {
+    title: "Responsive Web Development",
+    from: "Jan 22",
+    to: "Jun 22"
+  }
+];
+
+const projectrole = [
+  {
+    title: "Protyping and UX Design",
+    desc: "Design prototypes for data science visualization in Education platform"
+  },
+
+  {
+    title: "Data Visualization",
+    desc: " Development of Smart Visualization Graphs"
+  },
+  {
+    title: "Responsive Admin Dashboard Development with Data Visualization",
+    desc: " Development of Responsive Website with Data Visualization Facilities"
+  }
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
